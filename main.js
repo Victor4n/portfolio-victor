7fc73b60cba9e10efeaa9000869292b556824bf5
Victor <victoryanaa15@gmail.com>
2023-11-20 02:22:53 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+const typed = new Typed('.typed', {
+    strings: ['<i class="Victor">ictor Orihuela Yana</i>'],
+    typeSpeed: 95,
+    startDelay: 500,
+    backSpeed: 150,
+    backDelay: 3000,
+    loop: false,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '.',
+    contentType: 'html',
+});
+
+window.addEventListener("scroll", function () {
+    var header = document.getElementById("headerSticky");
+
+    if (window.scrollY > 0) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+});
+
+const section = document.querySelectorAll("section");
+
+const options = {
+    threshold : "0.6",
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(e => {
+        if (e.isIntersecting){
+            console.log(e.target.id)
+        }
+        
+    });
+}, options);
+
+section.forEach(section =>{
+    observer.observe(section);
+}) 
+
+
+
+//Habilidades
+
+const myTag = ['HTML', 'CSS', 'JavaScript', 'React', 'NextJS', 'Redux', 'TypeScript', 'Python', 'Django', 'Express', 'NodeJS', 'BootStrap', 'MogoDB', 'JQuery', 'SCSS', 'PUG'];
+
+var tagcloud = TagCloud('.content', myTag, {
+    radius: 200,
+    maxSpeed: 'slow',
+    initSpeed: 'normal',
+    direction: 135,
+    keep: true
+});
